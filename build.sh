f2a2428be8e806047f52a06195fac23c7046964d
i349934 <berry.ban@sap.com>
2020-02-22 20:14:33 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ BIN_NAME=e5go
 go build -o $BIN_NAME *.go
 cp $BIN_NAME /usr/bin/$BIN_NAME && chmod +x /usr/bin/$BIN_NAME
 
-cp $BIN_NAME /etc/systemd/system/$BIN_NAME.service
+cp $BIN_NAME.service /etc/systemd/system/
 
-systemctl daemon_reload
+systemctl daemon-reload
 systemctl enable $BIN_NAME
 systemctl start $BIN_NAME
